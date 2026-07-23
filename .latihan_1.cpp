21ddc004830c0f40a78df34a22d4005680f03e1a
BintangPrasetyo95 <sutpidotdash@gmail.com>
2026-07-23 21:00:27 +0700

    first commit

---
+++ .latihan_1.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world";
+    cout << endl;
+    cout << "daggers";
+    cout << endl;
+    cout << "neighbors";
+    cout << endl;
+    cout << "streamers";
+    cout << " 70";
+    cout << endl;
+
+    cout << "yo bro hello" << endl; 
+
+    int x = 7; int y = 3;
+
+    cout << "x adalah" << x;
+    cout << endl;
+    cout << "y itu apa?" << y;
+
+    int b = x + y;
+    cout << "x + b adalah " << b;
+    cout << "x + b adalah " << x + y;
+    cout << endl;
+
+    string namaku = "Banyua";
+
+    namaku = "Banyu";
+
+    cout << "namaku siapa ya = "<< namaku<<endl;
+    float t;
+    t = 356.78;
+    cout << "t / x = " << t / x << endl;
+    cout << "t + y * x = " << t + y * x;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    string papa;
+    cin >> papa;
+    cout << "halo! "<< papa <<endl;
+    cout << endl;
+    
+    int q1;
+    int q2;
+    cout << "q1 =" ;
+    cin >> q1;
+    cout << "q2 =" ;
+    cin >> q2;
+    cout << "Jadi q1 + q2 itu adalah " << q1 + q2<<endl;
+    int ww1;
+    int ww2;
+    cout << "ww1";
+    cin >> ww1;
+    cout << "ww2";
+    cin >> ww2;
+    cout << "ww1 x ww2 berapa si ===" << ww1 * ww2;
+}
