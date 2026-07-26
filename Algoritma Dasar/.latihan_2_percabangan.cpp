0b3415e53ef608ca33b11d584f067382f0e622f2
BanyuBiru254 <bluewater25454@gmail.com>
2026-07-26 21:36:19 +0700

    Latihan 2 Percabangan

---
+++ Algoritma Dasar/.latihan_2_percabangan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int y = 8;
+    int b = 4;
+
+    cout << "y = " << y;
+    cout << endl;
+    cout << "b = " << b;
+    cout << endl;
+    cout << "y x b?? = " << y * b << endl;
+
+    if (y > 5)
+    {
+        cout << "y lebih dari 5" << endl;
+    }
+    else
+    {
+        cout << "rtuyyf" << endl;
+    }
+}
