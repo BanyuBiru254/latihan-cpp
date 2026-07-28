9a9402ba2dd3a20852cd7bd219694c1e95d98180
BanyuBiru254 <bluewater25454@gmail.com>
2026-07-28 21:04:43 +0700

    fixxx

--- Algoritma Dasar/.latihan_2_percabangan.cpp
+++ Algoritma Dasar/.latihan_2_percabangan.cpp
@@ -6,20 +6,20 @@ int main()
     int y = 17;
     int b = 4;
 
-  // cout << "y = " << y;
-  //  cout << endl;
-  //  cout << "b = " << b;
-  //  cout << endl;
-  //  cout << "y x b?? = " << y * b << endl;
+    // cout << "y = " << y;
+    //  cout << endl;
+    //  cout << "b = " << b;
+    //  cout << endl;
+    //  cout << "y x b?? = " << y * b << endl;
 
-  //  if (y > 5)
-  //  {
- //       cout << "y lebih dari 5" << endl;
- //   }
-  //  else
- //   {
- //       cout << "rtuyyf" << endl;
- //   }
+    //  if (y > 5)
+    //  {
+    //       cout << "y lebih dari 5" << endl;
+    //   }
+    //  else
+    //   {
+    //       cout << "rtuyyf" << endl;
+    //   }
 
     // if (b < 4)
     // {
@@ -30,24 +30,25 @@ int main()
     //     cout << "b itu lebih dari 4 woi";
     // }
 
-    if (b < 4)
-    {
-        cout << "b kurang dari 4";
-    }
-    else if (b > 4)
-    {
-        cout << "b lebih dari 4";
-    }
-    else
-    {
-        cout << "b sama dengan 4";
-    }
+    //   if (b < 4)
+    //   {
+    //       cout << "b kurang dari 4";
+    //   }
+    //   else if (b > 4)
+    //{
+    //   cout << "b lebih dari 4";
+    //   }
+    //   else
+    //   {
+    //       cout << "b sama dengan 4";
+    // }
 
     if (y < 17)
     {
         cout << "aku berumur 16 tahun";
     }
-    else if (y > 17){
-        cout <<"aku berumur 18 tahun"
+    else if (y > 17)
+    {
+        cout << "aku berumur 18 tahun";
     }
 }
